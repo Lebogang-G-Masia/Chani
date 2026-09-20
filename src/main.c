f60db257a7649973dc142663d1ba1c9b064ae2e9
Lebogang Masia <lebogangmasia0@gmail.com>
2026-09-20 23:31:11 +0200

    Added Knight moves and captures

--- src/main.c
+++ src/main.c
@@ -10,7 +10,12 @@
 #include <stdio.h>
 
 int main() {
-    init();
-    return 0;
 
+    init();
+    parse_fen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R b KQkq - 0 1");
+    print_board();
+
+    generate_moves();
+
+    return 0;
 }
